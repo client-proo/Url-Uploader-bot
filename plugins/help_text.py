33d4e15a256d1577282c7bfdf098f685ad257d53
client-proo <client.proo2024@gmail.com>
2025-10-25 15:58:54 +0330

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
-# the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,85 +10,68 @@ logger = logging.getLogger(__name__)
 import os
 import sqlite3
 
-# the secret configuration specific things
+# تنظیمات
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
-# the Strings used for this "thing"
 from translation import Translation
-
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
-async def help_user(bot, update):
-    if update.from_user.id in Config.AUTH_USERS:
-        # logger.info(update)
-        await bot.send_message(
-            chat_id=update.chat.id,
+# --- دستور /help ---
+@pyrogram.Client.on_message(filters.command(["help"]) & filters.private)
+async def help_user(client, message: Message):
+    if message.from_user.id in Config.AUTH_USERS:
+        await message.reply_text(
             text=Translation.HELP_USER,
             parse_mode="html",
-            disable_web_page_preview=True,
-            reply_to_message_id=update.message_id
+            disable_web_page_preview=True
         )
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
-async def start(bot, update):
-    if update.from_user.id in (Config.AUTH_USERS & Config.LAZY_DEVELOPER):
-        # logger.info(update)
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.LAZY_DEVELOPER_TEXT.format(update.from_user.first_name),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("⚡️𝔖𝔲𝔭𝔭𝔬𝔯𝔱", url="https://t.me/LazyPrincessSupport"),
-                        InlineKeyboardButton("✪ ＹＴ ✪", url="https://youtube.com/@LazyDeveloperr"),
-                        InlineKeyboardButton("⚡️ U𝖕𝖉𝖆𝖙e", url="https://t.me/LazyDeveloper"),
-                    ],
-                    [InlineKeyboardButton("⭑💢 𝚂 𝙾 𝙲 𝚒 𝙰 𝙻 💢⭑", url="https://instagram.com/LazyDeveloper__")],
-                    [InlineKeyboardButton("🦋 ⭑┗━━┫⦀⦙ O W N E R ⦙⦀┣━━┛⭑ 🦋", url="https://t.me/LazyDeveloperr")],
-                ]
-            ),
-            reply_to_message_id=update.message_id
-        )
-    elif update.from_user.id in Config.AUTH_USERS:
-        # logger.info(update)
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.START_TEXT.format(update.from_user.first_name),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("⚡️𝔖𝔲𝔭𝔭𝔬𝔯𝔱", url="https://t.me/LazyPrincessSupport"),
-                        InlineKeyboardButton("✪ ＹＴ ✪", url="https://youtube.com/@LazyDeveloperr"),
-                        InlineKeyboardButton("⚡️ U𝖕𝖉𝖆𝖙e", url="https://t.me/LazyDeveloper"),
-                    ],
-                    [InlineKeyboardButton("⭑💢 𝚂 𝙾 𝙲 𝚒 𝙰 𝙻 💢⭑", url="https://instagram.com/LazyDeveloper__")],
-                    [InlineKeyboardButton("🦋 ⭑┗━━┫⦀⦙ O W N E R ⦙⦀┣━━┛⭑ 🦋", url="https://t.me/LazyDeveloperr")],
-                ]
-            ),
-            reply_to_message_id=update.message_id
-        )
+# --- دستور /start ---
+@pyrogram.Client.on_message(filters.command(["start"]) & filters.private)
+async def start(client, message: Message):
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+
+    # کیبورد مشترک برای کاربران مجاز
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("⚡️𝔖𝔲𝔭𝔭𝔬𝔯𝔱", url="https://t.me/LazyPrincessSupport"),
+                InlineKeyboardButton("✪ ＹＴ ✪", url="https://youtube.com/@LazyDeveloperr"),
+                InlineKeyboardButton("⚡️ U𝖕𝖉𝖆𝖙e", url="https://t.me/LazyDeveloper"),
+            ],
+            [InlineKeyboardButton("⭑💢 𝚂 𝙾 𝙲 𝚒 𝙰 𝙻 💢⭑", url="https://instagram.com/LazyDeveloper__")],
+            [InlineKeyboardButton("🦋 ⭑┗━━┫⦀⦙ O W N E R ⦙⦀┣━━┛⭑ 🦋", url="https://t.me/LazyDeveloperr")],
+        ]
+    )
+
+    # برای Lazy Developer
+    if user_id in (Config.AUTH_USERS & getattr(Config, "LAZY_DEVELOPER", set())):
+        text = Translation.LAZY_DEVELOPER_TEXT.format(first_name)
+        await message.reply_text(text, reply_markup=keyboard)
+
+    # برای کاربران مجاز
+    elif user_id in Config.AUTH_USERS:
+        text = Translation.START_TEXT.format(first_name)
+        await message.reply_text(text, reply_markup=keyboard)
+
+    # برای بقیه کاربران
     else:
-        # logger.info(update) ==         
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.LAZY_START_TEXT.format(update.from_user.first_name),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("🦋 ⭑┗━━┫⦀⦙ O W N E R ⦙⦀┣━━┛⭑ 🦋", url="https://t.me/LazyDeveloperr")],
-                    [
-                        InlineKeyboardButton("▍║▍▏║ UPDATE ║▍▏║▍", url="https://t.me/LazyPrincessSupport"),
-                    ],
-                    [InlineKeyboardButton("⭑💢 𝚂 𝙾 𝙲 𝚒 𝙰 𝙻 💢⭑", url="https://instagram.com/LazyDeveloper__")],
-                ]
-            ),
-            reply_to_message_id=message_id
+        text = Translation.LAZY_START_TEXT.format(first_name)
+        restricted_keyboard = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("🦋 ⭑┗━━┫⦀⦙ O W N E R ⦙⦀┣━━┛⭑ 🦋", url="https://t.me/LazyDeveloperr")],
+                [InlineKeyboardButton("▍║▍▏║ UPDATE ║▍▏║▍", url="https://t.me/LazyPrincessSupport")],
+                [InlineKeyboardButton("⭑💢 𝚂 𝙾 𝙲 𝚒 𝙰 𝙻 💢⭑", url="https://instagram.com/LazyDeveloper__")],
+            ]
         )
+        await message.reply_text(text, reply_markup=restricted_keyboard)
